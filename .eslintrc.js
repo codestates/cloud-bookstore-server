c26c1ac229319e5803da47aef88f7aa3917a5e29
paigekim29 <paigekim29@gmail.com>
2021-02-20 17:17:01 +0900

    [0]Create eslint, prettier, typescript
    husky 적용은 아직 안 된 상태입니다

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  settings: {
+    'import/resolver': { node: { extensions: ['.js', '.jsx', '.ts', '.tsx'] } },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-console': 'error', // 콘솔로그 error 처리
+    'semi': ['error', 'always'], // ; 반드시 존재해야 함
+  },
+  ignorePatterns: ['dist/', 'node_modules/', '.eslintrc.js'],
+};
