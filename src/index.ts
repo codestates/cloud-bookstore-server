0cf6e347d84f36832ce39d2a418330e9ed883650
paigekim29 <paigekim29@gmail.com>
2021-02-23 15:59:55 +0900

    [2-1]Create index.ts
    필요한 미들웨어 설치와 설정

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,39 @@
-import express from 'express';
-const app = express();
+import 'dotenv/config';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser, { json } from 'body-parser';
+import cookieParser from 'cookie-parser';
+import router from './routes/index';
 
-app.get('/', (req, res) => res.status(200).send('Hello??'));
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cookieParser());
+app.use(json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', router);
+
+app.use(
+  cors({
+    origin: [
+      'https://cloud-bookstore.com',
+      'https://www.cloud-bookstore.com',
+      'https://server.cloud-bookstore.com',
+    ],
+    methods: ['GET', 'POST', 'OPTIONS', 'PATCH', 'DELETE'],
+    credentials: true,
+  }),
+);
+
+app.get('/', (req: Request, res: Response) => res.status(200).send('구름책방'));
+
+//Handle Error
+app.use((err: Error, req: Request, res: Response) => {
+  res.status(500).send({
+    message: err.message,
+  });
+});
 
 // eslint-disable-next-line no-console
-app.listen(5000, () => console.log('Hello World!!!'));
+app.listen(PORT, () => console.log('Hello World!!!'));
