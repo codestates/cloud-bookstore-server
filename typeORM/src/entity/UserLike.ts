8d1a2f9066922090be62991cae5cb970bbefae3f
paigekim29 <paigekim29@gmail.com>
2021-02-25 10:30:29 +0900

    [2-3]Create model
    모델 생성 및 필요한 파일 생성

---
+++ typeORM/src/entity/UserLike.ts
@@ -0,0 +1,34 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+
+import { User } from './User';
+
+@Entity()
+export class UserLike {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  userId!: number;
+
+  @Column()
+  novelId!: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt!: Date;
+
+  @ManyToOne((type) => User, (user) => user.userLikes, {
+    nullable: false,
+    onDelete: 'CASCADE',
+  })
+  user!: User;
+}
