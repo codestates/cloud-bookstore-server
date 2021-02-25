5eea98e2c945d7de499e70924f6a3015ee3433ee
paigekim29 <paigekim29@gmail.com>
2021-02-25 15:02:27 +0900

    Update

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,8 +27,8 @@ module.exports = {
   },
   plugins: ['@typescript-eslint'],
   rules: {
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
+    'no-unused-vars': 'off', // 나중에 지우기
+    '@typescript-eslint/no-unused-vars': 'off', // 나중에 지우기
     'no-console': 'error', // 콘솔로그 error 처리
     'semi': ['error', 'always'], // ; 반드시 존재해야 함
     '@typescript-eslint/no-inferrable-types': 'off',
