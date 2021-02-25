7a7ef40d1dea67ddd895330fd1c3b0d87082155d
Sanghyuk Choi <nugu1223@gmail.com>
2021-02-25 13:19:35 +0900

    [2-3] Update Dir & Feature TypeORM

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,15 @@ import cors from 'cors';
 import bodyParser, { json } from 'body-parser';
 import cookieParser from 'cookie-parser';
 import router from './routes/index';
+import { createConnection } from 'typeorm';
+
+createConnection()
+  .then(() => {
+    console.log('typeorm connected');
+  })
+  .catch((err) => {
+    console.log('typeorm error', err);
+  });
 
 const app = express();
 const PORT = process.env.PORT;
