0fcadd88e6d0e1cb1294cbe0c34f5666580b721f
paigekim29 <paigekim29@gmail.com>
2021-02-26 13:48:55 +0900

    [3-7]Create edit comment on novel
    작품에 유저들이 코멘트를 남길 수 있도록 하는 코드 구축

--- src/controllers/cloud-bookstore/editNovelComment.ts
+++ src/controllers/cloud-bookstore/editNovelComment.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from 'express';
 
-export default (req: Request, res: Response): void => {
-  res.status(200).send(req.body);
+import NovelComment from '../../entity/NovelComment';
+
+export default async (req: Request, res: Response): Promise<void> => {
+  try {
+    const novelId: number = req.body.novelId;
+    const commentId: number = req.body.commentId;
+    const comment: string = req.body.comment;
+
+    const comments = await NovelComment.editComment(
+      commentId,
+      comment,
+    ).then(() => NovelComment.findByNovelId(novelId));
+
+    res.status(200).send({ message: 'successfully edited', comments });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
