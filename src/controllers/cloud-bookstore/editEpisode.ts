f47dc4d3d8903e2a95280c74c108aaf1fa78697f
Sanghyuk Choi <nugu1223@gmail.com>
2021-03-09 09:47:39 +0900

    test

--- src/controllers/cloud-bookstore/editEpisode.ts
+++ src/controllers/cloud-bookstore/editEpisode.ts
@@ -1,8 +1,16 @@
 import { Request, Response } from 'express';
+import Episode from '../../entity/Episode';
 
 export default async (req: Request, res: Response): Promise<void> => {
   try {
-    res.status(200).send(req.body);
+    const userId: number = req.cookies.userId;
+    const episodeId: number = req.body.id;
+    const title: string = req.body.title;
+    const category: string = req.body.category;
+    const description: string = req.body.description;
+    const thumbnail: string = req.body.thumbnail;
+
+    const novelId: number = await Episode.findByOnlyEpisodeId(userId);
   } catch (err) {
     res.status(500).send(err);
   }
